a07d06c220190146066502ab2f2bda1c2496f448
Victor Ochoa <victor20054@gmail.com>
2025-07-16 10:04:28 -0300

    passando o id para a abstração

--- src/CapituloZero.SharedKernel/Entity.cs
+++ src/CapituloZero.SharedKernel/Entity.cs
@@ -1,10 +1,15 @@
-﻿namespace CapituloZero.SharedKernel;
+﻿using System.Diagnostics.CodeAnalysis;
 
-public abstract class Entity
+namespace CapituloZero.SharedKernel;
+
+public abstract class Entity : IEqualityComparer<Entity>, IEquatable<Entity>
 {
+    protected Entity() { }
+
     private readonly List<IDomainEvent> _domainEvents = [];
 
     public List<IDomainEvent> DomainEvents => [.. _domainEvents];
+    public Guid Id { get; set; }
 
     public void ClearDomainEvents()
     {
@@ -14,5 +19,63 @@ public abstract class Entity
     public void Raise(IDomainEvent domainEvent)
     {
         _domainEvents.Add(domainEvent);
+    }
+
+    public override bool Equals(object obj)
+    {
+        var compareTo = obj as Entity;
+
+        if (ReferenceEquals(this, compareTo)) return true;
+        if (ReferenceEquals(null, compareTo)) return false;
+
+        return Id.Equals(compareTo.Id);
+    }
+
+    public static bool operator ==(Entity a, Entity b)
+    {
+        if (a is null && b is null)
+            return true;
+
+        if (a is null || b is null)
+            return false;
+
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(Entity a, Entity b)
+    {
+        return !(a == b);
+    }
+
+    public override int GetHashCode()
+    {
+        return (GetType().GetHashCode() ^ 93) + Id.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return $"{GetType().Name} [Id={Id}]";
+    }
+
+    public bool Equals(Entity? x, Entity? y)
+    {
+        if (x is null && y is null)
+            return true;
+        if (x is null || y is null)
+            return false;
+        return x.Equals(y);
+    }
+
+    public int GetHashCode([DisallowNull] Entity obj)
+    {
+        return obj.GetHashCode();
+    }
+
+    public bool Equals(Entity? other)
+    {
+        if (other is null)
+            return false;
+
+        return this.Equals(other as object);
     }
 }
---
+++ src/CapituloZero.SharedKernel/ValueObject.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapituloZero.SharedKernel;
+
+public abstract class ValueObject : IEqualityComparer<ValueObject>, IEquatable<ValueObject>
+{
+    public override bool Equals(object obj)
+    {
+        if (obj == null)
+            return false;
+
+        if (GetType() != obj.GetType())
+            return false;
+
+        var other = (ValueObject)obj;
+
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    /// <summary>
+    /// Needs to implement using a yield return statement to return each element one at a time
+    /// </summary>
+    /// <example>
+    /// yield return Foo;
+    /// yield return Bar;
+    /// </example>
+    /// <returns></returns>
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Select(x => x != null ? x.GetHashCode() : 0)
+            .Aggregate((x, y) => x ^ y);
+    }
+
+    public static bool operator ==(ValueObject a, ValueObject b)
+    {
+        return EqualOperator(a, b);
+    }
+
+    public static bool operator !=(ValueObject a, ValueObject b)
+    {
+        return NotEqualOperator(a, b);
+    }
+
+    protected static bool EqualOperator(ValueObject left, ValueObject right)
+    {
+        if (left is null ^ right is null)
+            return false;
+
+        return left is null || left.Equals(right);
+    }
+
+    protected static bool NotEqualOperator(ValueObject left, ValueObject right)
+    {
+        return !EqualOperator(left, right);
+    }
+
+    public bool Equals(ValueObject? other)
+    {
+        if (other is null)
+            return false;
+
+        return this.Equals(other as object);
+    }
+
+    public bool Equals(ValueObject? x, ValueObject? y)
+    {
+        if (x is null && y is null)
+            return true;
+        if (x is null || y is null)
+            return false;
+        return EqualOperator(x, y); ;
+    }
+
+    public int GetHashCode([DisallowNull] ValueObject obj) => obj.GetHashCode();
+
+}
